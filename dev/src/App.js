d93e8582f86e83e0ac823adfb9d7064f7a0bdd63
rahim <rahim.nagori@gmail.com>
2026-02-16 23:56:45 +0530

    folder structure updated

---
+++ dev/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <HomePage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
